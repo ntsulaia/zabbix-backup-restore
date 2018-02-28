a7d1795bdc5569f6ffe697c9f5e0a82026de0ca4
Nino Tsulaia <tsulaianino01@gmail.com>
2018-02-28 13:02:51 +0100

    Update zabbix_restore.py

--- app/zabbix_restore.py
+++ app/zabbix_restore.py
@@ -1,26 +1,56 @@
 import argparse
 from zabbixbackuputils import *
 from zabbixrestoreutils import *
+import sys
 
 
 #zabbix_restore() function calls restore functions from zabbixrestoreutils and restores either database or configuration
 #or both.
 
 def zabbix_restore(args):
-    print("Stopping Zabbix Server ...")
-    zabbix_server_stop()
-    if args.only_database:
-        zabbix_restore_database(args.file)
-        print(" **** Successfully restored Zabbix Server database! **** ")
-    elif args.only_config:
-        zabbix_restore_config(args.file)
-        print(" **** Successfully restored Zabbix Server configuration! **** ")
+    if not (args.file.endswith(".sql") or args.file.endswith(".tar.gz")):
+        print(" Unsupported file type!!! ")
+        sys.exit()
     else:
-        zabbix_restore_database(args.file)
-        zabbix_restore_config(args.file)
-        print(" **** Successfully restored Zabbix Server database and configuration! **** ")
-    print("Starting Zabbix Server ...")
-    zabbix_server_start()
+        if args.only_database:
+            print("Stopping Zabbix Server ...")
+            zabbix_server_stop()
+            print("Stopped ..." + "\n" + "Starting Zabbix Server Database restore ...")
+            zabbix_restore_database(args.file)
+            print(" **** Successfully restored Zabbix Server database! **** ")
+            print("Starting Zabbix Server ...")
+            zabbix_server_start()
+            print("Started!!!")
+        elif args.only_config:
+            if args.file.endswith(".sql"):
+                print("Please, provide archive file with .tar.gz extension ...")
+                sys.exit()
+            else:
+                print("Stopping Zabbix Server ...")
+                zabbix_server_stop()
+                print("Stopped ..." + "\n" + "Starting Zabbix Server configuration restore ...")
+                zabbix_restore_config(args.file)
+                print(" **** Successfully restored Zabbix Server configuration! **** ")
+                print("Starting Zabbix Server ...")
+                zabbix_server_start()
+                print("Started!!!")
+        else:
+            if args.file.endswith(".sql"):
+                print("Please, provide archive file with .tar.gz extension ...")
+                sys.exit()
+            else:
+                print("Stopping Zabbix Server ...")
+                zabbix_server_stop()
+                print("Stopped ..." + "\n" + "Starting Zabbix Server Database restore ...")
+                zabbix_restore_database(args.file)
+                print(" **** Successfully restored Zabbix Server database! **** ")
+                print("Starting Zabbix Server configuration restore ...")
+                zabbix_restore_config(args.file)
+                print(" **** Successfully restored Zabbix Server configuration! **** ")
+                print("Starting Zabbix Server ...")
+                zabbix_server_start()
+                print("Started!!!")
+
 
 
 #create cli argument parser using python argparse framework
