5c9b77bd6ec8a00abbbcfc3749e59f44e77e0aa5
Nino Tsulaia <tsulaianino01@gmail.com>
2018-02-28 13:02:14 +0100

    Update zabbix_backup.py

--- app/zabbix_backup.py
+++ app/zabbix_backup.py
@@ -7,18 +7,23 @@ from zabbixbackuputils import *
 
 def zabbix_backup_archive(args):
     backup_files = "config databases httpd" #defines directories to backup
-    cmd = "tar cvfz zabbix_backup_{0}.tar.gz {1} && /usr/bin/rm -rf {1}" #archiving command
+    cmd = "tar cvfz zabbix_backup_{0}.tar.gz {1} && rm -rf {1}" #archiving command
     time = str(datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")) #timestamp
     print("Stopping Zabbix Server ...")
     zabbix_server_stop()
+    print("Stopped ..." + "\n" + "Starting Zabbix Server Database backup ...")
     zabbix_database_backup(args.backup_path)
+    print("Finished Database backup ..." + "\n" + "Starting Zabbix Server configuration backup ...")
     zabbix_config_backup(args.backup_path)
+    print("Finished Configuration backup ..." + "\n" + "Starting Zabbix Server HTTPD Configuration backup ...")
     zabbix_httpd_backup(args.backup_path)
-    print("Starting Zabbix Server ...")
+    print("Zabbix Server backup completed ..." + "\n" + "Starting Zabbix Server ...")
     zabbix_server_start()
+    print("Started ..." + "\n" + "Archiving backup files ...")
     p = subprocess.Popen(cmd.format(time, backup_files), stdout=subprocess.PIPE, shell=True, cwd=args.backup_path)
     p.communicate()
     p.wait()
+    print("Archiving completed ..." + "\n" + "Removing old backup archives ...")
     zabbix_backups_rotate(args.backup_path, args.number_of_copies) #deletes old backup archives
     print(" **** Successfully finished Zabbix Server backup and archiving! **** ")
 
@@ -41,4 +46,3 @@ def backupfunc():
 
 if __name__ == "__main__":
     backupfunc()
-
