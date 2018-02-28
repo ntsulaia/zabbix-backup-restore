918432771119dc7f05a87d7df60b6b9bc47084f2
Nino Tsulaia <tsulaianino01@gmail.com>
2018-02-28 13:04:11 +0100

    Update zabbixrestoreutils.py

--- app/zabbixrestoreutils.py
+++ app/zabbixrestoreutils.py
@@ -5,14 +5,14 @@ import os
 #restores Zabbix Server database
 
 def zabbix_restore_database(filename):
-    cmd = "/usr/bin/mysql -u root < {0}"
+    cmd = "mysql -u root < {0}"
     directory = os.path.dirname(os.path.abspath(filename))
     if filename.endswith(".sql"):
         p = subprocess.Popen(cmd.format(os.path.abspath(filename)), stdout=subprocess.PIPE, shell=True)
         p.communicate()
         p.wait()
-    elif filename.endswith(".tar.gz"):
-        untar = "/usr/bin/tar xvfz {0}"
+    else:
+        untar = "tar xvfz {0}"
         p = subprocess.Popen(untar.format(filename), stdout=subprocess.PIPE, shell=True, cwd=directory)
         p.communicate()
         p.wait()
@@ -20,32 +20,26 @@ def zabbix_restore_database(filename):
         p = subprocess.Popen(cmd.format(filename), stdout=subprocess.PIPE, shell=True)
         p.communicate()
         p.wait()
-        rm = "/usr/bin/rm -rf config databases httpd"
+        rm = "rm -rf config databases httpd"
         p = subprocess.Popen(rm, stdout=subprocess.PIPE, shell=True, cwd=directory)
         p.communicate()
         p.wait()
-    else:
-        print(" Unsupported file type!!! ")
 
 
 #restores Zabbix Server configuration
 
 def zabbix_restore_config(filename):
-    cmd = "/usr/bin/cp -r {0}/config/zabbix /etc/ && /usr/bin/cp {0}/httpd/zabbix.conf /etc/httpd/conf.d/zabbix.conf"
-    untar = "/usr/bin/tar xvfz {0}"
-    rm = "/usr/bin/rm -rf databases httpd config"
+    cmd = "cp -r {0}/config/zabbix /etc/ && cp {0}/httpd/zabbix.conf /etc/httpd/conf.d/zabbix.conf"
+    untar = "tar xvfz {0}"
+    rm = "rm -rf databases httpd config"
     directory = os.path.dirname(os.path.abspath(filename))
-    if filename.endswith(".tar.gz"):
-        p = subprocess.Popen(untar.format(filename), stdout=subprocess.PIPE, shell=True, cwd=directory)
-        p.communicate()
-        p.wait()
-        p = subprocess.Popen(cmd.format(directory), stdout=subprocess.PIPE, shell=True)
-        p.communicate()
-        p.wait()
-        p = subprocess.Popen(rm, stdout=subprocess.PIPE, shell=True, cwd=directory)
-        p.communicate()
-        p.wait()
-    else:
-        print(" Unsupported file type!!! ")
-
+    p = subprocess.Popen(untar.format(filename), stdout=subprocess.PIPE, shell=True, cwd=directory)
+    p.communicate()
+    p.wait()
+    p = subprocess.Popen(cmd.format(directory), stdout=subprocess.PIPE, shell=True)
+    p.communicate()
+    p.wait()
+    p = subprocess.Popen(rm, stdout=subprocess.PIPE, shell=True, cwd=directory)
+    p.communicate()
+    p.wait()
 
