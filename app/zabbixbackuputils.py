fec4c91c7adaaa8aae9ed54272cc5998151a892e
Nino Tsulaia <tsulaianino01@gmail.com>
2018-02-28 13:03:40 +0100

    Update zabbixbackuputils.py

--- app/zabbixbackuputils.py
+++ app/zabbixbackuputils.py
@@ -4,7 +4,7 @@ import os
 #stops  Zabbix Server
 
 def zabbix_server_stop():
-    cmd = "/usr/bin/systemctl stop zabbix-server.service"
+    cmd = "systemctl stop zabbix-server.service"
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     p.communicate()
     p.wait()
@@ -13,7 +13,7 @@ def zabbix_server_stop():
 #starts Zabbix Server
 
 def zabbix_server_start():
-    cmd = "/usr/bin/systemctl start zabbix-server.service"
+    cmd = "systemctl start zabbix-server.service"
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
@@ -24,7 +24,7 @@ def zabbix_server_start():
 #backes up Zabbix Server database
 
 def zabbix_database_backup(backup_path):
-    cmd = "/usr/bin/mysqldump --all-databases > {0}/dump.sql"
+    cmd = "mysqldump --all-databases > {0}/dump.sql"
     backup_folder = "{0}/databases".format(backup_path)
     if not os.path.isdir(backup_folder):
         os.makedirs(backup_folder)
@@ -35,7 +35,7 @@ def zabbix_database_backup(backup_path):
 #backes up Zabbix Server config
 
 def zabbix_config_backup(backup_path):
-    cmd = "/usr/bin/cp -r /etc/zabbix {0}"
+    cmd = "cp -r /etc/zabbix {0}"
     backup_folder = "{0}/config/".format(backup_path)
     if not os.path.isdir(backup_folder):
         os.makedirs(backup_folder)
@@ -47,7 +47,7 @@ def zabbix_config_backup(backup_path):
 #backes up Zabbix httpd config
 
 def zabbix_httpd_backup(backup_path):
-    cmd = "/usr/bin/cp /etc/httpd/conf.d/zabbix.conf {0}"
+    cmd = "cp /etc/httpd/conf.d/zabbix.conf {0}"
     backup_folder = "{0}/httpd/".format(backup_path)
     if not os.path.isdir(backup_folder):
         os.makedirs(backup_folder)
